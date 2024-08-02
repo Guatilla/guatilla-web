a8f17525b31002a33ab184e43f6c2b3fe75cd451
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-08-03 01:50:36 +0200

    feat: under construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import UnderConstruction from './ui/under-construction';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello, World!</h1>
+      <UnderConstruction />
     </main>
   );
 }
---
+++ src/app/ui/under-construction.tsx
@@ -0,0 +1,14 @@
+// components/UnderConstruction.js
+
+export default function UnderConstruction() {
+    return (
+    <div className="bg-white p-10 rounded-lg shadow-lg text-center">
+        <h1 className="text-5xl font-bold text-yellow-500 mb-4">
+        Página en Construcción 👷🏼
+        </h1>
+        <p className="text-gray-700 text-lg">
+        Estamos trabajando arduamente para traerte algo increíble. ¡Vuelve pronto!
+        </p>
+    </div>
+);
+}
