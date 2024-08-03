e29f66875787073b074227548803b05b9f671ad1
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-08-04 00:33:27 +0200

    fix: under constructions

--- src/app/ui/under-construction.tsx
+++ src/app/ui/under-construction.tsx
@@ -3,12 +3,8 @@
 export default function UnderConstruction() {
     return (
         <div className="bg-white p-10 rounded-lg shadow-lg text-center">
-            <h1 className="text-5xl font-bold text-yellow-500 mb-4">
-            Página en Construcción 👷🏼
-            </h1>
-            <p className="text-gray-700 text-lg">
-            Estamos trabajando arduamente para traerte algo increíble. ¡Vuelve pronto!
-            </p>
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-yellow-500">Página en Construcción 👷🏼</h1>
+            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-700">Estamos trabajando arduamente para traerte algo increíble. ¡Vuelve pronto!</p>
         </div>
     );
 }
