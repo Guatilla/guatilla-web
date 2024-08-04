f70eedf6aa115188919cb32cfbe1cf0223b5ad88
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-08-04 23:01:18 +0200

    feat: link social media

--- src/app/ui/under-construction.tsx
+++ src/app/ui/under-construction.tsx
@@ -14,23 +14,23 @@ export default function UnderConstruction() {
             <div className="flex flex-col items-center sm:items-center">
                 <span className="md:text-2xl text-lg text-gray-600 font-semibold dark:text-blue-700">FOLLOW US</span>
                 <div className="flex flex-wrap justify-center sm:justify-start gap-4 mt-4 sm:mt-0 pt-4">
-                    <a href="#" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-[#1877f2] hover:opacity-75">
+                    <a href="https://www.facebook.com/WilkinsRuGo?locale=es_LA" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-[#1877f2] hover:opacity-75">
                         <Facebook />
                         Facebook
                     </a>
-                    <a href="#" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-[#000000] hover:opacity-75">
+                    <a href="https://x.com/Wilkins_Ruiz" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-[#000000] hover:opacity-75">
                         <X />
                         X
                     </a>
-                    <a href="#" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-gradient-to-r from-[#FED372] via-[#F86F2F] via-[#D43089] via-[#9E37B8] to-[#5F4BC6] hover:opacity-75">
+                    <a href="https://www.instagram.com/wilkinsruizgomez/" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-gradient-to-r from-[#FED372] via-[#F86F2F] via-[#D43089] via-[#9E37B8] to-[#5F4BC6] hover:opacity-75">
                         <Instagram />
                         Instagram
                     </a>
-                    <a href="#" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-[#0073b1] hover:opacity-75">
+                    <a href="https://www.linkedin.com/in/wilkins-ruiz-gomez-a18208234/" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-[#0073b1] hover:opacity-75">
                         <Linkedin />
                         Linkedin
                     </a>
-                    <a href="#" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-[#FF0000] hover:opacity-75">
+                    <a href="https://www.youtube.com/@wilkinsruizgomez7851" className="flex justify-center items-center gap-2 dark:text-white py-2 px-2 rounded bg-[#FF0000] hover:opacity-75">
                         <YouTube />
                         YouTube
                     </a>
